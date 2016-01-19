c2a9b84b2035dd714f297a4d4bd8e2247ef32854
ralren <delosreyes.ren@gmail.com>
2016-01-19 13:06:02 -0500

    installed handlebars and added in get functions

--- prowl.js
+++ prowl.js
@@ -5,12 +5,19 @@
 //https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
 //https://scotch.io/tutorials/setting-up-a-mean-stack-single-page-application
 var express = require('express');
+var handlebars = require('express-handlebars')
 var app = express();
 var bodyParser = require('body-parser');
 var redis = require('redis');
 var youtube = require('youtube-api');
+var db = require('./server.js');
 
 app.set('port', process.env.PORT || 3000);
+
+//set up handlebars view engine
+var view = handlebars.create({ defaultLayout:'main' });
+app.engine('handlebars', view.engine);
+app.set('view engine', 'handlebars');
 
 //configure app to use bodyParser() for getting data from POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,9 +25,29 @@ app.use(bodyParser.json());
 
 //ROUTES FOR PROWL FRONTEND
 //=======================================================================
-//test route to make sure everything is working
-app.get('/', function(req, res) {
-    res.send('hooray! welcome to the website!');
+
+app.get('/prowlers', function(req, res) {
+    db.getProwlers(function(err, docs) {
+      res.json({prowlers: docs});
+    });
+});
+
+app.get('/channels', function(req, res) {
+    db.getChannels('ldelosreyes@smith.edu', function(err, docs) {
+      res.json({channels: docs});
+    });
+});
+
+app.get('/keywords', function(req, res) {
+    db.getKeywords('ldelosreyes@smith.edu', 'chaoticmonki', function(err, docs) {
+      res.json({keywords: docs});
+    });
+});
+
+app.get('/channelID', function(req, res) {
+    db.getChannelID('chaoticmonki', function(err, docs) {
+      res.json({playlistID: docs});
+    });
 });
 
 
--- server.js
+++ server.js
@@ -4,23 +4,82 @@
 //=======================================================================
 //http://www.sitepoint.com/using-redis-node-js/
 
+//set up connection to redis
 var redis = require('redis');
 var client = redis.createClient();
 
-client.smembers('prowler', function(err, reply) {
-    console.log(reply);
-});
+var youtube = require('youtube-api');
 
 
-//add to channel
-//add to prowler
-//add channel to prowler:[email]
-//add keywords to prowler:[email]
+// GET functions
+//=======================================================================
+
+//Fetch all emails/prowlers
+exports.getProwlers = function(cb) {
+  client.smembers('prowler', function(err, prowlers) {
+    if (err) {
+      console.log(err);
+    } else {
+      cb(null, prowlers);
+    }
+  });
+};
+
+//Fetch all channels for an email/prowler
+exports.getChannels = function(email, cb) {
+  var key = 'prowler:'.concat(email);
+  client.smembers(key, function(err, channels) {
+    if (err) {
+      console.log(err);
+    } else {
+      cb(null, channels);
+    }
+  });
+}
+
+//Fetch all keys for an email/prowler
+exports.getKeywords = function(email, channel, cb) {
+  var key = 'prowler:'.concat(email, ':', channel);
+  client.smembers(key, function(err, keywords) {
+    if (err) {
+      console.log(err);
+    } else {
+      cb(null, keywords);
+    }
+  });
+}
+
+exports.getChannelID = function(channel, cb) {
+  var key = 'channel:'.concat(channel);
+  client.get(key, function(err, playlistID) {
+    if (err) {
+      console.log(err);
+    } else {
+      cb(null, playlistID);
+    }
+  })
+}
+
+// DELETE FUNCTIONS
+//=======================================================================
 
 //delete prowler
 //delete channel from prowler:[email]
 //delete keywords from prowler:[email]
 
-//fetch channel's playlist id
-//fetch email's channels
-//fetch a prowler's channel's keywords
+// POST FUNCTIONS
+//=======================================================================
+
+/*exports.addChannel = function(channel, cb) {
+  client.sadd(['channel', channel], function(err, reply) {
+    if (err) {
+      console.log(err);
+    } else {
+      addChannelID(channel, function())
+    }
+  })
+}*/
+//add to channel
+//add to prowler
+//add channel to prowler:[email]
+//add keywords to prowler:[email]
